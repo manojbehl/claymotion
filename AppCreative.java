1da045daa86982fd3af821e6176e72fc3ab16473
manojbehl <manoj.behl@gmail.com>
2016-12-27 23:45:56 +0530

    Add files via upload

---
+++ AppCreative.java
@@ -0,0 +1,105 @@
+package com.claymotion.advertiser;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.claymotion.webservice.client.WebServiceClient;
+
+public class AppCreative {
+
+	private String app_id;
+	private String version;
+	private String name;
+	private String short_description;
+	private double rating;
+	private String developer;
+	private String description;
+	private String icon;
+	private List<Creative> creatives;
+
+	public String getApp_id() {
+		return app_id;
+	}
+
+	public void setApp_id(String app_id) {
+		this.app_id = app_id;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getShort_description() {
+		return short_description;
+	}
+
+	public void setShort_description(String short_description) {
+		this.short_description = short_description;
+	}
+
+	public double getRating() {
+		return rating;
+	}
+
+	public void setRating(double rating) {
+		this.rating = rating;
+	}
+
+	public String getDeveloper() {
+		return developer;
+	}
+
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getIcon() {
+		return icon;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public List<Creative> getCreatives() {
+		return creatives;
+	}
+
+	public void setCreatives(List<Creative> creatives) {
+		this.creatives = creatives;
+	}
+
+	public static void main(String[] args) throws Exception {
+		WebServiceClient webServiceClient = WebServiceClient.getSharedInstance();
+		Map<String ,String> hashMap = new HashMap<String, String>();
+		hashMap.put("Content-Type", "application/json");
+
+		
+		AppCreative appCreative = (AppCreative)webServiceClient.executeGetMethod("http://creatives.downloadapps.in/api/creative/com.flipkart.android",
+				AppCreative.class, hashMap);
+		
+		System.err.println( appCreative.getCreatives().size());
+		
+	}
+}
---
+++ Creative.java
@@ -0,0 +1,102 @@
+package com.claymotion.advertiser;
+
+public class Creative {
+	
+	private String _id;
+	private int width;
+	private int height;
+	private String title;
+	private String description;
+	private String app_id;
+	private String partner_id;
+	private boolean is_active;
+	private String mimetype;
+	private long size;
+	private String filename;
+	private String uploadPath;
+	private int __v;
+	public String get_id() {
+		return _id;
+	}
+	public void set_id(String _id) {
+		this._id = _id;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getApp_id() {
+		return app_id;
+	}
+	public void setApp_id(String app_id) {
+		this.app_id = app_id;
+	}
+	public String getPartner_id() {
+		return partner_id;
+	}
+	public void setPartner_id(String partner_id) {
+		this.partner_id = partner_id;
+	}
+	public boolean isIs_active() {
+		return is_active;
+	}
+	public void setIs_active(boolean is_active) {
+		this.is_active = is_active;
+	}
+	
+	public long getSize() {
+		return size;
+	}
+	public void setSize(long size) {
+		this.size = size;
+	}
+	
+	public String getUploadPath() {
+		return uploadPath;
+	}
+	public void setUploadPath(String uploadPath) {
+		this.uploadPath = uploadPath;
+	}
+	public int get__v() {
+		return __v;
+	}
+	public void set__v(int __v) {
+		this.__v = __v;
+	}
+	public String getMimetype() {
+		return mimetype;
+	}
+	public void setMimetype(String mimetype) {
+		this.mimetype = mimetype;
+	}
+	public String getFilename() {
+		return filename;
+	}
+	public void setFilename(String filename) {
+		this.filename = filename;
+	}
+	
+	
+	
+
+}
